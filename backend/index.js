3f9a4be15032bb0bce8898629c3fd67324c87040
Jayendra <jayendrabamne68@gmail.com>
2026-06-27 10:52:14 +0530

    Update backend CORS

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://flood-snowy.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
